94b1ce963c427b2f1b34f4297128ea8bfb9c0958
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-09-28 22:32:35 +0300

    add modal component

---
+++ src/components/ModalEncryptDescrypt.vue
@@ -0,0 +1,20 @@
+<template>
+    <div @click.self="closeModal" class="w-full h-full fixed bg-modalbg">
+        <div class="flex flex-col items-center text-center max-w-md my-6 mx-auto bg-white p-6 rounded-lg relative">
+            <slot name="form"></slot>
+
+            <span @click="closeModal"
+                class="cursor-pointer absolute top-0 right-0 m-4 px-1 border rounded hover:bg-gray-100 duration-300">X</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+}
+</script>
